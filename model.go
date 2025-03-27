7da8ce5be0a4660d0d8f4f8d594e402ec040a53f
Bmixo <mail@bmixo.com>
2025-03-27 14:33:32 +0800

    update model and trainer

--- model.go
+++ model.go
@@ -1,6 +1,9 @@
 package torch
 
-import "github.com/Jimmy2099/torch/data_struct/tensor"
+import (
+	"fmt"
+	"github.com/Jimmy2099/torch/data_struct/tensor"
+)
 
 // Model 定义模型接口
 type ModelInterface interface {
@@ -11,15 +14,28 @@ type ModelInterface interface {
 
 type Model struct {
 	_      ModelInterface
+	_      TrainerInterface
 	Layers []Layer
+	//
+	LayerIndex2Name map[int]string
+	LayerName2Index map[string]int
+	//
 }
 
 func NewModel() *Model {
-	return &Model{}
+	return &Model{
+		Layers:          []Layer{},
+		LayerIndex2Name: map[int]string{},
+		LayerName2Index: map[string]int{},
+	}
 }
+
 func (m *Model) AddLayer(layer Layer) {
 	m.Layers = append(m.Layers, layer)
+	m.LayerIndex2Name[len(m.Layers)-1] = fmt.Sprint(len(m.Layers) - 1)
+	m.LayerName2Index[fmt.Sprint(len(m.Layers)-1)] = len(m.Layers) - 1
 }
+
 func (m *Model) Forward(input *tensor.Tensor) *tensor.Tensor {
 	return m.Forward(input)
 }
@@ -37,7 +53,7 @@ func (m *Model) ZeroGrad() {
 }
 
 // Trainer 定义训练器接口
-type Trainer interface {
+type TrainerInterface interface {
 	Train(model ModelInterface, inputs, targets *tensor.Tensor, epochs int, learningRate float64)
 	Validate(model ModelInterface, inputs, targets *tensor.Tensor) float64
 }
