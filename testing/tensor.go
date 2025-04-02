fc693795aeb75715b11e3da5aac50f9bf7fa725f
Bmixo <mail@bmixo.com>
2025-04-02 16:32:52 +0800

    update

--- testing/tensor.go
+++ testing/tensor.go
@@ -3,9 +3,7 @@ package testing
 import (
 	"fmt"
 	"github.com/Jimmy2099/torch/data_struct/tensor"
-	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -89,7 +87,7 @@ out = process_data(in1,in2)
 save_tensor_to_csv(out,"%s")
 `, inputPath1, inputPath2, inPyScript, outPutPath)
 
-	RunPyscript(pythonScript)
+	RunPyScript(pythonScript)
 
 	// 读取计算结果
 	outTensor, err := tensor.LoadFromCSV(outPutPath)
@@ -97,61 +95,4 @@ save_tensor_to_csv(out,"%s")
 		panic(err)
 	}
 	return outTensor
-}
-
-func RunPyscript(pythonScript string) {
-	// 打印嵌入的 Python 脚本内容
-	fmt.Println("Embedded Python script:\n", pythonScript)
-
-	// 将嵌入的 Python 脚本写入临时文件
-	file, err := os.CreateTemp("", "script.*.py")
-	if err != nil {
-		panic(err)
-	}
-	defer os.Remove(file.Name()) // 程序结束时删除临时文件
-
-	// 将嵌入的 Python 脚本内容写入临时文件
-	_, err = file.WriteString(pythonScript)
-	if err != nil {
-		panic(err)
-	}
-
-	// 执行 Python 脚本
-	cmd := exec.Command("python", file.Name())
-
-	// 实时打印输出
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(fmt.Sprintln("Error creating Stdout pipe:", err))
-	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		panic(fmt.Sprintln("Error creating Stderr pipe:", err))
-	}
-
-	// 启动命令
-	if err := cmd.Start(); err != nil {
-		panic(fmt.Sprintln("Error starting Python script:", err))
-	}
-
-	// 使用 Goroutines 来实时打印标准输出和错误输出
-	go func() {
-		_, err := io.Copy(os.Stdout, stdout)
-		if err != nil {
-			panic(fmt.Sprintln("Error copying stdout:", err))
-		}
-	}()
-
-	go func() {
-		_, err := io.Copy(os.Stderr, stderr)
-		if err != nil {
-			panic(fmt.Sprintln("Error copying stderr:", err))
-		}
-	}()
-
-	// 等待命令执行完成
-	if err := cmd.Wait(); err != nil {
-		panic(fmt.Sprintln("Error waiting for Python script to finish:", err))
-	}
 }
