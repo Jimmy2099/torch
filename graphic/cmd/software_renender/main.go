9f4d1d646b3985e711de559fe04e161d233c0946
Bmixo <mail@bmixo.com>
2025-04-03 15:43:03 +0800

    update

--- graphic/cmd/software_renender/main.go
+++ graphic/cmd/software_renender/main.go
@@ -19,25 +19,23 @@ import (
 	"time"
 )
 
-//	glm "gitlab.com/brickhill/site/fauxgl"
-//
-// 渲染器结构体，包含绘图参数和缓冲区信息
+// glm "gitlab.com/brickhill/site/fauxgl"
 type Render struct {
-	p         *plot.Plot
-	width     int
-	height    int
-	frameBuff *image.RGBA
-	camera    *tensor.Tensor
-	lookAt    *tensor.Tensor
-	up        *tensor.Tensor
-	fovy      float64
-	near      float64
-	far       float64
-	scale     float64
-	ZBuffer   [][]float64
+	p          *plot.Plot
+	width      int
+	height     int
+	frameBuff  *image.RGBA
+	GFrameBuff *ebiten.Image
+	camera     *tensor.Tensor
+	lookAt     *tensor.Tensor
+	up         *tensor.Tensor
+	fovy       float64
+	near       float64
+	far        float64
+	scale      float64
+	ZBuffer    [][]float64
 }
 
-// 初始化渲染器，设置默认参数和缓冲区
 func (m *Render) Init() {
 	m.p = plot.New()
 	m.height = 1080
@@ -53,16 +51,14 @@ func (m *Render) Init() {
 		m.scale = 12
 	}
 	{
-		// 初始化深度缓冲区
 		m.ZBuffer = make([][]float64, m.width)
 		for i := range m.ZBuffer {
 			m.ZBuffer[i] = make([]float64, m.height)
 			for j := range m.ZBuffer[i] {
-				m.ZBuffer[i][j] = 1 // 深度范围默认为 0 到 1
+				m.ZBuffer[i][j] = 1
 			}
 		}
 	}
-	// 初始化帧缓冲区
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{m.width, m.height}
 	m.frameBuff = image.NewRGBA(image.Rectangle{upLeft, lowRight})
@@ -73,7 +69,6 @@ func (m *Render) Init() {
 	}
 }
 
-// 添加矢量数据到帧缓冲区
 func (m *Render) AddVecData(data []*tensor.Tensor) {
 	fill := color.RGBA{R: 255, G: 0, B: 0, A: 255} // 设置绘制颜色为红色
 	for i := 0; i < len(data); i++ {
@@ -81,10 +76,9 @@ func (m *Render) AddVecData(data []*tensor.Tensor) {
 	}
 }
 
-// 将帧缓冲区内容保存为 PNG 图像
 func (m *Render) Draw() {
-	f, _ := os.Create(fmt.Sprint(time.Now().Unix()) + ".png") // 使用当前时间戳命名文件
-	png.Encode(f, imaging.FlipV(m.frameBuff))                 // 垂直翻转图像后保存
+	f, _ := os.Create(fmt.Sprint(time.Now().Unix()) + ".png")
+	png.Encode(f, imaging.FlipV(m.frameBuff))
 }
 
 func NewVec3(x, y, z float64) *tensor.Tensor {
@@ -103,7 +97,6 @@ func (r *Render) drawLine(v0, v1 *tensor.Tensor) []*tensor.Tensor {
 	return result
 }
 
-// 绘制两点之间的线条（忽略深度信息）
 func (m *Render) drawLineWithoutZBuff(v0 *tensor.Tensor, v1 *tensor.Tensor) (result []*tensor.Tensor) {
 	for t := float64(0); t < 1; t += 0.01 {
 		x := v0.X() + (v1.X()-v0.X())*t
@@ -113,15 +106,12 @@ func (m *Render) drawLineWithoutZBuff(v0 *tensor.Tensor, v1 *tensor.Tensor) (res
 	return
 }
 
-// 绘制三角形边框
 func (m *Render) drawTriangle(t0 *tensor.Tensor, t1 *tensor.Tensor, t2 *tensor.Tensor) (result []*tensor.Tensor) {
-	result = append(result, m.drawLine(t0, t1)...) // t0 到 t1 的线段
-	result = append(result, m.drawLine(t1, t2)...) // t1 到 t2 的线段
-	result = append(result, m.drawLine(t2, t0)...) // t2 到 t0 的线段
+	result = append(result, m.drawLine(t0, t1)...)
+	result = append(result, m.drawLine(t1, t2)...)
+	result = append(result, m.drawLine(t2, t0)...)
 	return
 }
-
-var m *Render
 
 func main() {
 	//for test
@@ -131,7 +121,10 @@ func main() {
 	go func() {
 		http.ListenAndServe("localhost:6060", nil)
 	}()
+
+	var m *Render
 	g := ui.UIMain(nil, nil)
+
 	m = &Render{}
 	m.Init()
 
@@ -145,10 +138,6 @@ func main() {
 		panic(err)
 	}
 
-	//Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)
-	//t1 := NewVec3(10, 70, 0)
-	//t2 := NewVec3(50, 160, 0)
-	//t3 := NewVec3(70, 80, 0)
 	var data3 []*tensor.Tensor
 	for _, face := range objData.Faces {
 		data3 = append(data3, m.drawTriangle(NewVec3(face.Points[0].Vertex.X, face.Points[0].Vertex.Y, face.Points[0].Vertex.Z),
@@ -161,16 +150,6 @@ func main() {
 	viewport := tensor.Viewport(0, 0, float64(1), float64(1))
 	view := tensor.LookAt(m.camera, m.lookAt, m.up)
 	var matrix *tensor.Tensor
-	//if false {
-	//	//record exec time
-	//	model = model.Rotate(tensor.NewTensor([]float64{1, 0, 0}, []int{3}), glm.Radians(5))
-	//	matrix = projection.MatMulMatrix(view).MatMulMatrix(viewport).MatMulMatrix(model)
-	//	recordExecTime(&matrix, data3)
-	//	os.Exit(0)
-	//}
-	//m.Draw()
-	//a := app.New()
-	//w := a.NewWindow("Images")
 	go func() {
 		for {
 			view = tensor.LookAt(m.camera, m.lookAt, m.up)
@@ -182,10 +161,10 @@ func main() {
 
 			data4 = m.Scaling(data4)
 			m.AddVecData(data4)
-			frameBuff = ebiten.NewImageFromImage(imaging.FlipV(m.frameBuff))
+			m.GFrameBuff = ebiten.NewImageFromImage(imaging.FlipV(m.frameBuff))
 			//img := canvas.NewImageFromImage(imaging.FlipV(m.frameBuff))
 			if g != nil {
-				g.FrameBuff = frameBuff
+				g.FrameBuff = m.GFrameBuff
 			}
 			//w.SetContent(img)
 			//time.Sleep(time.Millisecond * 10)
@@ -205,16 +184,12 @@ func main() {
 			}
 		}
 	}()
-	g.FrameBuff = frameBuff
+	g.FrameBuff = m.GFrameBuff
 	g.Camera = m.camera
 	g.RunGame()
-	//w.Resize(fyne.NewSize(640, 480))
-	//w.ShowAndRun()
+
 }
 
-var frameBuff *ebiten.Image
-
-// 应用视图变换到点集
 func Camera(matrix *tensor.Tensor, v []*tensor.Tensor) (result []*tensor.Tensor) {
 	for i := 0; i < len(v); i++ {
 		vx := matrix.MulPosition(v[i])
@@ -223,7 +198,6 @@ func Camera(matrix *tensor.Tensor, v []*tensor.Tensor) (result []*tensor.Tensor)
 	return
 }
 
-// 缩放点集到帧缓冲区范围
 func (m *Render) Scaling(v []*tensor.Tensor) (result []*tensor.Tensor) {
 	for i := 0; i < len(v); i++ {
 		result = append(result, NewVec3(float64(m.width)*((v[i].X()-0.5)/m.scale+0.5),
--- graphic/software-renender/README.md
+++
@@ -1,1 +0,0 @@
-my course project small software renender
